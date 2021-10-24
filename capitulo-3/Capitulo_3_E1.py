25dc9fe8abb57f8bba56863c1380e08fdfd964e5
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2021-10-24 17:36:47 -0300

    Mudanças e adições
    Foi modificada a classe Node
    Foi modificada a função bfs
    Foi adicionado mais dois metodos de busca, o dfs e o idfs
    Foi adicionada uma função para encontrar o caminho para a solução

--- capitulo-3/Capitulo_3_E1.py
+++ capitulo-3/Capitulo_3_E1.py
@@ -1,113 +1,177 @@
+from collections import deque
+import timeit
+import resource
 
 class Node():
-
-    def __init__(self, posicoesAtual, estado=None, profundidade=None):
-        self.posicoesAtual = posicoesAtual
-        if estado == None:
-            self.movimento = "Inicio"
-        else:
-            self.movimento = estado
-        if profundidade == None:
-            self.profundidade = 0
-        else:
-            self.profundidade = profundidade
-        self.meta = self.verificarMeta()
+    def __init__(self, tabela, noPai, movimento, profundidade):
+        self.tabela = tabela
+        self.movimento = movimento
+        self.profundidade = profundidade
+        self.pai = noPai
         self.nosFilho = list()
-        #self.imprimirPuzzle()
+        self.ehMeta = self.__verificarMeta()
+        self.map = self.__map()
 
+    def imprimirPuzzle(self):
+        #print(f'Movimento: {self.movimento}\nProfundidade: {self.profundidade}')
+        print(f'| {self.tabela[0]} {self.tabela[1]} {self.tabela[2]} |')
+        print(f'| {self.tabela[3]} {self.tabela[4]} {self.tabela[5]} |')
+        print(f'| {self.tabela[6]} {self.tabela[7]} {self.tabela[8]} |\n')
 
-    def moverParaEsquerda(self):
-        if self.posicaoZero() % 3 > 0 and self.movimento != 'Direita':
-            novo = self.posicoesAtual.copy()
-            novo = self.trocarPos(novo, -1)
-            self.nosFilho.append(Node(novo, 'Esquerda', self.profundidade + 1))
+    def gerarFilhos(self):
+        posZero = self.__posicaoZero()
 
-    def moverParaDireita(self):
-        if self.posicaoZero() % 3 < 2 and self.movimento != 'Esquerda':
-            novo = self.posicoesAtual.copy()
-            novo = self.trocarPos(novo, 1)
-            self.nosFilho.append(Node(novo, 'Direita', self.profundidade + 1))
+        if posZero - 3 >= 0 and self.movimento != 'Cima':
+            novaTabela = self.tabela.copy()
+            novaTabela[posZero], novaTabela[posZero - 3] = novaTabela[posZero - 3], novaTabela[posZero]
+            self.nosFilho.append(Node(novaTabela, self, 'Baixo', self.profundidade + 1))
 
-    def moverParaCima(self):
-        if self.posicaoZero() - 3 >= 0 and self.movimento != 'Baixo':
-            novo = self.posicoesAtual.copy()
-            novo = self.trocarPos(novo, -3)
-            self.nosFilho.append(Node(novo, 'Cima', self.profundidade + 1))
+        if posZero + 3 < len(self.tabela) and self.movimento != 'Baixo':
+            novaTabela = self.tabela.copy()
+            novaTabela[posZero], novaTabela[posZero + 3] = novaTabela[posZero + 3], novaTabela[posZero]
+            self.nosFilho.append(Node(novaTabela, self, 'Cima', self.profundidade + 1))
 
-    def moverParaBaixo(self):
-        if self.posicaoZero() + 3 < len(self.posicoesAtual) and self.movimento != 'Cima':
-            novo = self.posicoesAtual.copy()
-            novo = self.trocarPos(novo, 3)
-            self.nosFilho.append(Node(novo, 'Baixo', self.profundidade + 1))
+        if posZero % 3 > 0 and self.movimento != 'Esquerda':
+            novaTabela = self.tabela.copy()
+            novaTabela[posZero], novaTabela[posZero - 1] = novaTabela[posZero - 1], novaTabela[posZero]
+            self.nosFilho.append(Node(novaTabela, self, 'Direita', self.profundidade + 1))
 
-    def trocarPos(self, vetor, orientacao):
-        posZero = self.posicaoZero()
-        temp = vetor[posZero]
-        vetor[posZero] = vetor[posZero + (orientacao)]
-        vetor[posZero + (orientacao)] = temp
-        return vetor
+        if posZero % 3 < 2 and self.movimento != 'Direita':
+            novaTabela = self.tabela.copy()
+            novaTabela[posZero], novaTabela[posZero + 1] = novaTabela[posZero + 1], novaTabela[posZero]
+            self.nosFilho.append(Node(novaTabela, self, 'Esquerda', self.profundidade + 1))
 
-    def posicaoZero(self):
+    def __posicaoZero(self):
         i = 0
-        while i < len(self.posicoesAtual):
-            if self.posicoesAtual[i] == 0:
+        while i < len(self.tabela):
+            if self.tabela[i] == 0:
                 break
             i += 1
         return i
 
-    def imprimirPuzzle(self):
-        print(f'{self.movimento}\nProfundidade: {self.profundidade}')
-        print(f'| {self.posicoesAtual[0]} {self.posicoesAtual[1]} {self.posicoesAtual[2]} |')
-        print(f'| {self.posicoesAtual[3]} {self.posicoesAtual[4]} {self.posicoesAtual[5]} |')
-        print(f'| {self.posicoesAtual[6]} {self.posicoesAtual[7]} {self.posicoesAtual[8]} |\n')
+    def __verificarMeta(self):
+        return self.tabela == [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-    def gerarFilhos(self):
-        if not self.meta:
-            self.moverParaEsquerda()
-            self.moverParaBaixo()
-            self.moverParaDireita()
-            self.moverParaCima()
+    def __map(self):
+        return ''.join(str(val) for val in self.tabela)
 
-    def verificarMeta(self):
-        return self.posicoesAtual == [0,1,2,3,4,5,6,7,8]
 
-    def __str__(self): # Vai ser utilizado para o dicionario
-        return ''.join(str(val) for val in self.posicoesAtual) + self.movimento + str(self.profundidade)
+def bfs(noInicial):
+    visitados, fronteira = set(), deque([noInicial])
+    max_front = 0
+    while fronteira:
+        noAtual = fronteira.popleft()
+        visitados.add(noAtual.map)
 
-def bfs(no):
-    fronteira = [no]                                            # Fronteira da busca
-    nosPai = dict()                                             # Dicionario para armazenar os nós pai dos nós filho
-    visitados = [no]                                            # Armazena os nós já visitados
-    res = []                                                    # Vai armazenar o caminho
-    achou = False
+        if noAtual.ehMeta:
+            return [noAtual, len(fronteira), max_front, len(visitados)]
 
-    while not achou:                                            # Só para quando achar solução
-        pai = fronteira[0]
-        del fronteira[0]
+        noAtual.gerarFilhos()
+        for vizinhoAtual in noAtual.nosFilho:
+            if vizinhoAtual.map not in visitados:
+                fronteira.append(vizinhoAtual)
+                visitados.add(vizinhoAtual.map)
 
-        if pai.meta:                                            # Verifica se achou o estado final do puzzle
-            node = pai                                          # node vai ser utilizado para reconstruir o caminho
-            node.imprimirPuzzle()
-            while node.posicoesAtual != no.posicoesAtual:       # Volta até o estado inicial
-                res.append(node.movimento)                      # Armazena todos os movimentos
-                node = nosPai[str(node)]                        # node recebe o nó pai
-            res.reverse()                                       # Inverte para ficar na posição certa
-            print(f'Solução: {res}')
-            achou = True
+        if len(fronteira) > max_front:
+            max_front = len(fronteira)
 
-        pai.gerarFilhos()                                       # Gera os nós filhos
-        for filho in pai.nosFilho:                              # Para cada filho gerado, ira verificar se já foi
-            if filho not in visitados:                          # visitado. Se não foi vai ser adicionado na lista
-                visitados.append(filho)                         # e vai ser adicionado no dicionario o pai com base
-                nosPai[str(filho)] = pai                        # no filho.
-                fronteira.append(filho)                         # Adiciona o filho na fronteira
+def dfs(noInicial):
+    visitados, fronteira = set(), list([noInicial])
+    max_front = 0
 
+    while fronteira:
+        noAtual = fronteira.pop()
+        visitados.add(noAtual.map)
+
+        if noAtual.ehMeta:
+            return [noAtual, len(fronteira), max_front, len(visitados)]
+
+        noAtual.gerarFilhos()
+        for vizinhoAtual in reversed(noAtual.nosFilho):
+            if vizinhoAtual.map not in visitados:
+                fronteira.append(vizinhoAtual)
+                visitados.add(vizinhoAtual.map)
+
+        if len(fronteira) > max_front:
+            max_front = len(fronteira)
+
+def idfs(noInicial):
+     max_alt = 1
+     while True:
+        visitados, fronteira = set(), list([noInicial])
+        max_front = 0
+        while fronteira:
+            noAtual = fronteira.pop()
+            visitados.add(noAtual.map)
+
+            if noAtual.ehMeta:
+                return [noAtual, len(fronteira), max_front, len(visitados)]
+
+            noAtual.gerarFilhos()
+            for vizinhoAtual in reversed(noAtual.nosFilho):
+                if vizinhoAtual.map not in visitados:
+                    if vizinhoAtual.profundidade <= max_alt:
+                        fronteira.append(vizinhoAtual)
+                        visitados.add(vizinhoAtual.map)
+
+            if len(fronteira) > max_front:
+                max_front = len(fronteira)
+
+        max_alt += 1
+
+def obterCaminho(noFinal, noInicial):
+    sol = []
+    while noFinal.tabela != noInicial.tabela:
+        sol.append(noFinal.movimento)
+        #noFinal.imprimirPuzzle()
+        noFinal = noFinal.pai
+    sol.reverse()
+    print('Caminho:', end=' ')
+    print(sol)
 
 def executar():
 
-    puzzle = Node([3, 1, 2, 6, 0, 5, 7, 4, 8])
+    print('Insira a tabela e o método de busca da seguinte forma: bfs x,x,x,x,x,x,x,x,x')
+    entrada = input()
+
+    metodo = entrada.split(' ')[0]
+    dados = entrada.split(' ')[1].split(',')
+    tabela = []
+    result = None
+    tudoCerto = True
+    for val in dados:
+        tabela.append(int(val))
+
+    puzzle = Node(tabela, None, None, 0)
+    print('Entrada:')
     puzzle.imprimirPuzzle()
-    bfs(puzzle)
+
+    comeco = timeit.default_timer()
+
+    if metodo == 'bfs':
+        result = bfs(puzzle)
+    elif metodo == 'dfs':
+        result = bfs(puzzle)
+    elif metodo == 'idfs':
+        result = idfs(puzzle)
+    else:
+        print('Método Inválido')
+        tudoCerto = False
+
+    fim = timeit.default_timer() - comeco
+
+    if tudoCerto:
+        print('Saida:')
+        result[0].imprimirPuzzle()
+        obterCaminho(result[0], puzzle)
+        print(f'Custo do caminho: {result[0].profundidade}\n'
+              f'Nós visitados: {result[3]}\n'
+              f'Tamanho da fronteira: {result[1]}\n'
+              f'Tamanho máximo da fronteira: {result[2]}\n'
+              f'Profundidade da pesquisa: {result[0].profundidade}\n'
+              f'Profundidade da pesquisa máxima: {result[0].profundidade + 1}\n'
+              f'Tempo de execução: {fim:.5f}\n'
+              f'Máxima memória ram usada: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0}')
 
 if __name__ == "__main__":
     executar()
